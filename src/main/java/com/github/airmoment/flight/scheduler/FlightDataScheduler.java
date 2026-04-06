23b88f2bc4459e4d4ac95c27468d2b8fe94083f8
seahee <lollol0617@gmail.com>
2026-04-06 10:43:38 +0900

    [refactor] #22 - 동적 날짜를 반영하여 데이터 수집하도록 수정

--- src/main/java/com/github/airmoment/flight/scheduler/FlightDataScheduler.java
+++ src/main/java/com/github/airmoment/flight/scheduler/FlightDataScheduler.java
@@ -26,8 +26,7 @@ public class FlightDataScheduler {
 
 	private static final String ORIGIN = "ICN";
 	private static final List<String> TARGET_AIRPORTS = List.of("CDG", "JFK", "SYD");
-	private static final List<LocalDate> OUTBOUND_DATES = List.of(
-		LocalDate.of(2026,5,15),
+	private static final List<LocalDate> FIXED_OUTBOUND_DATES = List.of(
 		LocalDate.of(2026, 7, 25),
 		LocalDate.of(2026, 11, 15),
 		LocalDate.of(2026, 12, 25)
@@ -35,27 +34,31 @@ public class FlightDataScheduler {
 
 	@Scheduled(cron = "0 0 0,12 * * *", zone = "Asia/Seoul")  // 매일 00시, 12시
 	public void collectFlightData() {
+
+		// 동적으로 변하는 출발일자 : 현재로부터 한 달 후
+		LocalDate dynamicOutboundDate = LocalDate.now().plusMonths(1);
 		log.info("항공편 데이터 수집 시작: {}", LocalDateTime.now());
 
 		try {
-			collectForPeriod();
+			collectForPeriod(dynamicOutboundDate);
 			log.info("항공편 데이터 수집 완료");
 		} catch (Exception e) {
 			log.error("항공편 데이터 수집 실패: {}", e.getMessage());
 		}
 	}
 
-	private void collectForPeriod() {
+	private void collectForPeriod(LocalDate dynamicOutboundDate) {
 		for (String targetAirport : TARGET_AIRPORTS) {
-			for (LocalDate outboundDate : OUTBOUND_DATES) {
-				FlightSearch outboundFlightSearch = flightDataService.saveFlightSearch(ORIGIN, targetAirport,
-					outboundDate);
-				FlightSearchResponse outboundResponse = serpApiClient.fetchFlights(
-					ORIGIN, targetAirport, outboundDate.toString()
-				);
-				log.info("outboundResponse: {}", outboundResponse);
-				flightDataService.saveFlights(outboundFlightSearch, outboundResponse, FlightDirection.OUTBOUND);
+			fetchAndSaveData(targetAirport, dynamicOutboundDate);
+			for (LocalDate outboundDate : FIXED_OUTBOUND_DATES) {
+				fetchAndSaveData(targetAirport, outboundDate);
 			}
 		}
+	}
+
+	private void fetchAndSaveData(String targetAirport, LocalDate outboundDate) {
+		FlightSearch outboundFlightSearch = flightDataService.saveFlightSearch(ORIGIN, targetAirport, outboundDate);
+		FlightSearchResponse outboundResponse = serpApiClient.fetchFlights(ORIGIN, targetAirport, outboundDate.toString());
+		flightDataService.saveFlights(outboundFlightSearch, outboundResponse, FlightDirection.OUTBOUND);
 	}
 }
