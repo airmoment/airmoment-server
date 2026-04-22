5965f86e11d5e93a2089888c94b86e3338c8af03
seahee <lollol0617@gmail.com>
2026-04-23 02:42:11 +0900

    [feat] #24 - feature vector 계산을 위해 수집된 데이터를 조회하는 리포지토리 메서드 구현

--- src/main/java/com/github/airmoment/flight/repository/FlightOfferRepository.java
+++ src/main/java/com/github/airmoment/flight/repository/FlightOfferRepository.java
@@ -1,10 +1,35 @@
 package com.github.airmoment.flight.repository;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.github.airmoment.flight.domain.FlightOffer;
+import com.github.airmoment.flight.domain.enums.FlightDirection;
 
 @Repository
 public interface FlightOfferRepository extends JpaRepository<FlightOffer, Long> {
+
+	@Query("SELECT MIN(fo.price) FROM FlightOffer fo "
+		+ "WHERE fo.flightSearch.departureAirportCode = :dep "
+		+ "AND fo.flightSearch.arrivalAirportCode = :arr "
+		+ "AND fo.flightSearch.outboundDate = :date "
+		+ "AND fo.direction = :direction "
+		+ "AND fo.flightSearch.searchedAt <= :now "
+		+ "GROUP BY fo.flightSearch.id "
+		+ "ORDER BY MAX(fo.flightSearch.searchedAt) DESC")
+	List<Integer> findRecentMinOutboundPrices(
+		@Param("dep") String dep,
+		@Param("arr") String arr,
+		@Param("date") LocalDate date,
+		@Param("direction") FlightDirection direction,
+		@Param("now") LocalDateTime now,
+		Pageable pageable
+	);
 }
--- src/main/java/com/github/airmoment/flight/repository/FlightPriceHistoryRepository.java
+++ src/main/java/com/github/airmoment/flight/repository/FlightPriceHistoryRepository.java
@@ -1,10 +1,31 @@
 package com.github.airmoment.flight.repository;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.github.airmoment.flight.domain.FlightPriceHistory;
 
 @Repository
 public interface FlightPriceHistoryRepository extends JpaRepository<FlightPriceHistory, Long> {
+
+	@Query("SELECT fph.price FROM FlightPriceHistory fph "
+		+ "WHERE fph.flightSearch.departureAirportCode = :dep "
+		+ "AND fph.flightSearch.arrivalAirportCode = :arr "
+		+ "AND fph.flightSearch.outboundDate = :date "
+		+ "AND fph.flightSearch.searchedAt <= :now "
+		+ "ORDER BY fph.timeStamp DESC")
+	List<Integer> findLatestHistoryPrices(
+		@Param("dep") String dep,
+		@Param("arr") String arr,
+		@Param("date") LocalDate date,
+		@Param("now") LocalDateTime now,
+		Pageable pageable
+	);
 }
