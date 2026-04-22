36dc56ad7a040217f95860d4c89f8d89665f85f9
seahee <lollol0617@gmail.com>
2026-04-23 04:23:20 +0900

    [feat] #24 - 예측모델 응답 dto, 항공권 조회 시 예측 response dto 생성

--- src/main/java/com/github/airmoment/flight/dto/FlightListResponse.java
+++ src/main/java/com/github/airmoment/flight/dto/FlightListResponse.java
@@ -3,10 +3,11 @@ package com.github.airmoment.flight.dto;
 import java.util.List;
 
 public record FlightListResponse(
+	FlightPredictDto predict,
 	int totalCount,
 	List<FlightItemResponse> flightList
 ) {
-	public static FlightListResponse of(List<FlightItemResponse> items) {
-		return new FlightListResponse(items.size(), items);
+	public static FlightListResponse of(FlightPredictDto predict, List<FlightItemResponse> items) {
+		return new FlightListResponse(predict, items.size(), items);
 	}
 }
