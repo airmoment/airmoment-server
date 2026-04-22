417ded2958c072feefff0251e3273cc948f1a1d9
seahee <lollol0617@gmail.com>
2026-04-23 04:23:58 +0900

    [feat] #24 - 필요 필드에 notnull 제약 추가, snakecase로 json 직렬화 추가

--- src/main/java/com/github/airmoment/flight/dto/FlightFeatureVector.java
+++ src/main/java/com/github/airmoment/flight/dto/FlightFeatureVector.java
@@ -1,15 +1,35 @@
 package com.github.airmoment.flight.dto;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import jakarta.validation.constraints.NotNull;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record FlightFeatureVector(
+	@NotNull
 	String routeId,
+	@NotNull
+	String departureAirportCode,
+	@NotNull
+	String arrivalAirportCode,
+	String outboundDate,
+	@NotNull
 	String searchedDayOfWeek,
+	@NotNull
 	int daysToDeparture,
+	@NotNull
 	boolean isWeekendSearch,
+	@NotNull
 	boolean isLongHaul,
+	@NotNull
 	int offerCount,
+	@NotNull
 	float nonstopRatio,
 	Integer cheapestNonstopPrice,
+	@NotNull
 	boolean cheapestOfferHasLayover,
+	@NotNull
 	int currentCheapestPrice,
 	Integer currGapToTypicalMin,
 	Integer currGapToTypicalMax,
