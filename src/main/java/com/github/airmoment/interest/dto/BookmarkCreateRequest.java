ee9c25f4dfec62896538b1de46ae3206196861c9
seahee <lollol0617@gmail.com>
2026-05-07 01:25:14 +0900

    [feat] #32 - 관심노선 설정/조회를 위한 dto 생성

---
+++ src/main/java/com/github/airmoment/interest/dto/BookmarkCreateRequest.java
@@ -0,0 +1,18 @@
+package com.github.airmoment.interest.dto;
+
+import java.time.LocalDate;
+
+import com.github.airmoment.flight.domain.enums.AirportCode;
+
+import jakarta.validation.constraints.NotNull;
+
+public record BookmarkCreateRequest (
+	@NotNull
+	AirportCode departureCode,
+	@NotNull
+	AirportCode arrivalCode,
+	@NotNull
+	LocalDate departureAt,
+	@NotNull
+	boolean nonstopOnly
+) {}
